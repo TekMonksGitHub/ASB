0c831d51e3bba94df22124cdc948f1776a9ae196
Rohit Kapoor <rvkapoor@tekmonks.com>
2018-09-13 00:53:59 -0400

    Added support for message consumers.

--- routes/csvfilereader.js
+++ routes/csvfilereader.js
@@ -8,6 +8,4 @@ const fs = require("fs");
 
 exports.start = (routeName, listener, messageContainer, message) => {
     LOG.info(`[CSVFILEREADER] Processing CSV file: ${message[CONSTANTS.MSGCONSTANTS.FILEPATH]}`);
-    messageContainer.remove(message);
-    
 }
