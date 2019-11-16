247f17f54839f0f1a64deab20f8b18f890d95c45
Rohit Kapoor <rvkapoor@tekmonks.com>
2019-11-16 00:42:45 -0500

    Allow disabling the log for performance tests etc.

--- routes/csvwriter.js
+++ routes/csvwriter.js
@@ -12,6 +12,7 @@ exports.start = (routeName, csvwriter, _messageContainer, message) => {
     if (message.env[routeName] && message.env[routeName].isBeingProcessed) return;    // already working on it.
     if (!message.env[routeName]) message.env[routeName] = {}; message.env[routeName].isBeingProcessed = true;
     message.setGCEligible(false);
+    LOG.debug(`[CSVWRITER] Processing message with timestamp: ${message.timestamp}`);
 
     let handleError = e => {
         LOG.error(`[CSVWRITER] ${e}`); message.addRouteError(routeName); message.setGCEligible(true); return;}
