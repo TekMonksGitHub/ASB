202df6e51d396465a3054fcd70e817038d7326bc
Kapil-Gurnani <kapilgurnani4@gmail.com>
2020-05-05 12:20:17 +0530

    add maxFileOpens and fix memory crash issue

--- routes/filereader.js
+++ routes/filereader.js
@@ -10,25 +10,39 @@ const utils = require(CONSTANTS.LIBDIR+"/utils.js");
 
 exports.start = (routeName, filereader, _messageContainer, message) => {
     if (message.env[routeName] && message.env[routeName].isBeingProcessed) return;    // already working on it.
-    if (!message.env[routeName]) message.env[routeName] = {}; message.env[routeName].isBeingProcessed = true;
     message.setGCEligible(false);
+    
+    if(!ESB.env.totalMemory) ESB.env.totalMemory = 0;
+    if(ESB.env.totalMemory > filereader.totalMax)return;
+    
+    if(!filereader.flow.env.fileNum) filereader.flow.env.fileNum = 0;
+    if(filereader.flow.env.fileNum > filereader.maxFileOpens) return;
+    
+    if (!message.env[routeName]) message.env[routeName] = {}; message.env[routeName].isBeingProcessed = true;
+    
+    filereader.flow.env.fileNum++;
 
     let handleError = e => {
-        LOG.error(`[FILEREADER] ${e}`); message.addRouteError(routeName); message.setGCEligible(true); return;}
+        LOG.error(`[FILEREADER] ${e}`); message.addRouteError(routeName); message.setGCEligible(true); filereader.flow.env.fileNum--; return;}
 
     let handleReadResult = (e, data) => {
         if (e) handleError(`Read error: ${e}`); else {
+            if(ESB.env.totalMemory > filereader.totalMax) {message.env[routeName].isBeingProcessed = false; filereader.flow.env.fileNum--; return;}
+            ESB.env.totalMemory+=data.length/1000000;
             try{message.content = JSON.parse(data);} catch(e){message.content = data;}
+            filereader.flow.env.fileNum--;
             message.addRouteDone(routeName);
             message.setGCEligible(true);
             delete message.env[routeName].isBeingProcessed; // clean our garbage
+            LOG.info('[FILEREADER] success');
 
             if (filereader.donePath) try {
                 let newPath = `${filereader.donePath}/${path.basename(message.env.filepath)}.${utils.getTimeStamp()}`;
-                fs.rename(message.env.filepath, newPath, err => {if (err) LOG.error(`[FILEREADER] Error moving: ${err}`)});
-            } catch (e) {LOG.error(`[FILEREADER] Error moving: ${e}`);}
+                fs.rename(message.env.filepath, newPath, err => {if (err) LOG.error(`[FILEREADER] Error moving: ${err}`); });
+            } catch (err) {LOG.error(`[FILEREADER] Error moving: ${err}`);}
         }
     }
 
     fs.readFile(message.env.filepath||filereader.path, filereader.encoding?filereader.encoding:null, handleReadResult);
+
 }
