ab5a24a1768b32b49c3487cb277d26ca9e2511c2
md arbaaz <arba689@gmail.com>
2024-06-27 16:35:09 +0530

    Modified the http_listener to handle Binary Data

--- routes/js.js
+++ routes/js.js
@@ -6,6 +6,8 @@
 const utils = require(`${CONSTANTS.LIBDIR}/utils.js`);
 
 exports.start = (routeName, js, messageContainer, message) => {
+    if (message.env[routeName]?.isProcessing) return;
+    if (!message.env[routeName]) message.env[routeName] = {isProcessing: true};
     LOG.info(`[ROUTE_JS] Processing message with timestamp: ${message.timestamp}`);
 
     if (js.module) {
