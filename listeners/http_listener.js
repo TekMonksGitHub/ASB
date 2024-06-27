0e6aa32455b08ef71ff2d771212e2d791832f500
md arbaaz <arba689@gmail.com>
2024-06-27 10:40:00 +0530

    Modified http_listener to handle binary data as well

--- listeners/http_listener.js
+++ listeners/http_listener.js
@@ -1,21 +1,21 @@
 /* 
  * http_listener.js, HTTP listener - for any incoming HTTP requests.
  * 
- * (C) 2018 TekMonks. All rights reserved.
+ * (C) 2024 TekMonks. All rights reserved.
  */
 
 const urlMod = require("url");
 const httpServerFactory = require(`${CONSTANTS.LIBDIR}/httpServerFactory.js`);
 
 exports.start = (routeName, listener, messageContainer) => {
-    if ((listener.flow.env[routeName] && listener.flow.env[routeName].server) || 
-    (listener.flow.env[routeName] && listener.flow.env[routeName].creatingServer)) return; // already listening or creating
+    if ((listener.flow.env[routeName] && listener.flow.env[routeName].server) ||
+        (listener.flow.env[routeName] && listener.flow.env[routeName].creatingServer)) return; // already listening or creating
 
-   listener.flow.env[routeName]= {"creatingServer" : true};
+    listener.flow.env[routeName]= {"creatingServer" : true};
 
 
     httpServerFactory.createHTTPServer(listener, (err, server) => {
-        if (err) { 
+        if (err) {
             LOG.error(`[HTTP_LISTENER] Unable to create server due to ${err}, disabling the flow`);
             delete listener.flow.env[routeName].creatingServer; throw (err);
         }
@@ -26,24 +26,26 @@ exports.start = (routeName, listener, messageContainer) => {
         listener.flow.env[routeName].server.on("request", (req, res) => {
             const endPoint = urlMod.parse(req.url, true).pathname;
             if (endPoint != listener.url) return;   // not ours to handle
-    
-            let data = "";
-            req.on("data", chunk => data += chunk);
-            
+
+            const data = [];
+            req.on("data", chunk => data.push(chunk));
+
             req.on("end", _ => {
                 const message = MESSAGE_FACTORY.newMessageAllocSafe();
                 if (!message) {
-                    LOG.error("[HTTP_LISTENER] Message creation error, throttling listener."); 
+                    LOG.error("[HTTP_LISTENER] Message creation error, throttling listener.");
                     res.writeHead(429, {"Content-Type": "text/plain"});
                     res.write("Throttled.\n");
                     res.end();
                 } else {
                     message.env.http_listener = {listener, req, res};
-                    message.content = data;
+                    message.content = listener.contentType?.toLowerCase() === "binary"
+                        ? Buffer.concat(data)
+                        : Buffer.concat(data).toString("utf8");
                     message.addRouteDone(routeName);
                     messageContainer.add(message);
                     LOG.info(`[HTTP_LISTENER] Injected new message with timestamp: ${message.timestamp}`);
-                    LOG.debug(`[HTTP_LISTENER] Incoming request: ${data}`);
+                    LOG.debug(`[HTTP_LISTENER] Incoming request: ${message.content}`);
                 }
             });
         });
