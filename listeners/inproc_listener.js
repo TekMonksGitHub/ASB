27c6e3a8efa18c11fd705e60496b2b9f0a92c18f
TekmonksGitHub <rvkapoor@tekmonks.com>
2025-11-03 03:51:26 +0900

    Refactor for in-process flow control.

--- listeners/inproc_listener.js
+++ listeners/inproc_listener.js
@@ -1,21 +1,34 @@
 /** 
  * inproc_listener.js, In-process listener. Useful for software that embeds
- * the ASB inside the same process. 
+ * the ASB inside the same process. Property needed in flow definition is id.
+ * ID must be unique for each flow.
  * 
  * Usage: 
  * First ensure the flow which embeds this listener is running. Then
  *  const inproc_listener = require("path_to_listener/inproc_listener.js"); 
- *  inproc_listener.inject(message_content, response_receiver_function);
+ *  inproc_listener.inject(id, {messageContent: message_content, responseReceiver:response_receiver_function});
  * 
  * (C) 2025 TekMonks. All rights reserved.
  */
 
-let _injector;
+const IN_PROC_MB = "inproc_mb"; 
+
+_createInProcMB();
+function _createInProcMB() {
+    if (global.ESB.env[IN_PROC_MB]) return; // already exists
+    global.ESB.env[IN_PROC_MB] = {
+        subscribers: [],
+        publish: message => {for (const subscriber of subscribers) subscriber.onmessage(message)},
+        subscribe: subscriber => subscribers.push(subscriber)
+    }
+}
 
 exports.start = (routeName, listener, messageContainer) => {
     if (listener.flow.env[routeName]) return;   // already listening
     else {
-        _injector = (messageContent, responseReceiver) => { // create our message injector
+        global.ESB.env[IN_PROC_MB].subscribe({onmessage: idmessage => {
+            if (idmessage.id != listener.id) return;  // not for us
+            const {messageContent, responseReceiver} = idmessage.content;
             const message = MESSAGE_FACTORY.newMessageAllocSafe();
             if (!message) {
                 ASBLOG.error("[INPROC_LISTENER] Message creation error, throttling listener."); 
@@ -28,9 +41,9 @@ exports.start = (routeName, listener, messageContainer) => {
                 ASBLOG.info(`[INPROC_LISTENER] Injected new message with timestamp: ${message.timestamp}`);
                 return true;
             }
-        }
+        }});
         listener.flow.env[routeName] = true;
     }
 }
 
-exports.injector = injector;
+exports.inject = (id, content) => global.ESB.env[IN_PROC_MB].publish({id, content});
